8085c65f377a453baf2a376e870e98a426c9a747
kenedyCO <zombie-322@yandex.ru>
2023-07-28 14:56:29 +0300

    tg add

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+	t := mustToken()
+
+	// tgClient = telegram.New(token)
 	// fetcher = fetcher.New()
 	// processor = processor.New()
 	// Fconsumer.Start(fetcher, processor)
+}
+
+func mustToken() string {
+	token := flag.String("token-bot-token", "", "token for tg bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+	return *token
 }
